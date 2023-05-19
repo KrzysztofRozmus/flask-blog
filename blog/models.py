ac98319ac8a8846bcaa998e5bcebac2d6ff5cb95
KrzysztofRozmus <krzysztof@examplemail.com>
2023-05-19 21:12:12 +0200

    Convert posts to html, add to database & display on home page

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,9 @@ from flask_admin.contrib.sqla import ModelView
 from flask_login import current_user
 from flask import abort
 from flask_pagedown.fields import PageDownField
+from markdown import markdown
+import bleach
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -28,6 +31,10 @@ class Post(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120), nullable=False)
     content = db.Column(db.Text, nullable=False)
+
+    # Stores markdown post text as HTML
+    content_in_html = db.Column(db.Text, nullable=False)
+
     date_posted = db.Column(db.DateTime, nullable=False, default=current_datetime)
     author = db.Column(db.String, nullable=False, default="Admin")
 
@@ -38,6 +45,16 @@ class Post(db.Model, UserMixin):
 
     def __repr__(self):
         return f"User('{self.title}', '{self.content}', '{self.author}')"
+
+    # Method converts markdown post text as HTML to database in body_html column.
+    @staticmethod
+    def on_changed_body(target, value, oldvalue, initiator):
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
+                        'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
+                        'h1', 'h2', 'h3', 'p']
+        target.content_in_html = bleach.linkify(bleach.clean(
+            markdown(value, output_format='html'),
+            tags=allowed_tags, strip=True))
 
 
 # Grants access to Admin Panel only to Admin.
@@ -56,24 +73,24 @@ class UserView(ModelView):
             return abort(404)
 
 
-class PostView(ModelView):    
+class PostView(ModelView):
     # Extends default admin/model/create.html template.
-    create_template = "post_create.html"
-    
-    # Extends default admin/model/edit.html template.    
-    edit_template = "post_edit.html"
-    
+    create_template = "admin/post_create.html"
+
+    # Extends default admin/model/edit.html template.
+    edit_template = "admin/post_edit.html"
+
     # Replacing TextAreaField for PageDownField.
     def scaffold_form(self):
         form_class = super(PostView, self).scaffold_form()
         form_class.content = PageDownField()
         return form_class
-    
-    column_exclude_list = ["content"]
-    form_excluded_columns = ["author", "date_posted"]
+
+    column_exclude_list = ["content", "content_in_html"]
+    form_excluded_columns = ["author", "date_posted", "content_in_html"]
     form_widget_args = {"content": {"rows": 20}}
-    
-    
-    
+
+
 admin.add_view(UserView(User, db.session))
 admin.add_view(PostView(Post, db.session))
+db.event.listen(Post.content, 'set', Post.on_changed_body)
