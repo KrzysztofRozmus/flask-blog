c03a1d96c5768d6f57867cc69fc746711a6c9c3b
KrzysztofRozmus <krozmus101@gmail.com>
2023-08-06 21:59:50 +0200

    Add user authentication

--- blog/__init__.py
+++ blog/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 import os
 from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 load_dotenv(".env")
 
@@ -14,6 +15,12 @@ db = SQLAlchemy()
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///user.db"
 db.init_app(app)
 
+# User authentication
+login_manager = LoginManager()
+login_manager.init_app(app)
+# If user wants to access login_required page without being logged in, will be redirect to login page.
+login_manager.login_view = "login"
+login_manager.login_message_category = "danger"
 
 
 from blog import routes
--- blog/routes.py
+++ blog/routes.py
@@ -1,8 +1,15 @@
-from blog import app, db
+from blog import app, db, login_manager
 from flask import render_template, redirect, url_for, flash
 from blog.forms.auth import SignupForm, LoginForm
 from blog.models.user import User
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
+from datetime import timedelta
+
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(id)
 
 
 @app.route("/")
@@ -14,6 +21,10 @@ def home():
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
     form = SignupForm()
+
+    if current_user.is_authenticated and current_user.is_active:
+        flash("You are signed up and logged in already", "info")
+        return redirect(url_for("user_dashboard"))
 
     if form.validate_on_submit():
         user = User(username=form.username.data,
@@ -33,12 +44,16 @@ def signup():
 def login():
     form = LoginForm()
 
-    if form.validate_on_submit():
+    if current_user.is_authenticated and current_user.is_active:
+        flash("You are logged in already", "info")
+        return redirect(url_for("user_dashboard"))
+
+    elif form.validate_on_submit():
         user = db.session.execute(db.select(User).filter_by(email=form.email.data)).scalar()
-        password = check_password_hash(user.password,
-                                       form.password.data)
+        password = check_password_hash(user.password, form.password.data)
 
         if password:
+            login_user(user, remember=True, duration=timedelta(minutes=1))
             flash(f"Successfully logged in. Welcome {user.username} :)", "success")
             return redirect(url_for("user_dashboard"))
 
@@ -47,6 +62,17 @@ def login():
     return render_template("auth/login.html", form=form)
 
 
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+
+    flash("You have been log out.", "info")
+    return redirect(url_for("login"))
+
+
 @app.route("/user_dashboard", methods=["GET", "POST"])
+@login_required
 def user_dashboard():
+
     return render_template("user/dashboard.html")
